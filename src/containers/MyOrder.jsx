75f375bdc51c354c4db8e7b5e2468d04e7ed5547
YuneidyC <yuneidycastillo25@gmail.com>
2022-07-19 15:44:38 +0100

    Fix overflow MyOrder to my-order-content

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -2,12 +2,10 @@ import React, { useContext } from 'react';
 import OrderItem from '@components/OrderItem';
 import AppContext from '../context/AppContext';
 import '@styles/MyOrder.scss';
-import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
     const { state } = useContext(AppContext);
 
-    // Si se necesita utilizar esta funcion en otros lados se puede hacer un Hook y asi poder hacerlo mas amigable
     const sumTotal = () => {
         const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
         const sum = state.cart.reduce(reducer, 0);
@@ -24,16 +22,16 @@ const MyOrder = () => {
                 {state.cart.map((product, index) => (
                     <OrderItem product={product} key={index} index={index} />
                 ))}
-                <div className="order">
-                    <p>
-                        <span>Total</span>
-                    </p>
-                    <p>${sumTotal()}</p>
-                </div>
-                <button className="primary-button">
-                    Checkout
-                </button>
             </div>
+            <div className="order">
+                <p>
+                    <span>Total</span>
+                </p>
+                <p>${sumTotal()}</p>
+            </div>
+            <button className="primary-button">
+                Checkout
+            </button>
         </aside>
     );
 }
