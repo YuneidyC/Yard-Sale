03a548409e6e117b77e79badc7ac0404c56cc0bc
YuneidyC <yuneidycastillo25@gmail.com>
2022-08-07 15:50:46 +0100

    Fix media queries mobile to desktop

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '@styles/HeaderMenu.scss';
+import Header from './Header';
 
 const HeaderMenu = () => {
     return (
         <div className="Menu-header">
+            <h3>CATEGORIES</h3>
             <ul>
                 <li>
                     <a href="/">All</a>
@@ -24,6 +26,23 @@ const HeaderMenu = () => {
                     <a href="/">Others</a>
                 </li>
             </ul>
+            <div className='separator'></div>
+            <div className='account-user'>
+                <ul>
+                    <li>
+                        <a href="/" className="title">My orders</a>
+                    </li>
+                    <li>
+                        <a href="/">My account</a>
+                    </li>
+                    <li>
+                        platzi@example.com
+                    </li>
+                    <li>
+                        <a href="/">Sign out</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
