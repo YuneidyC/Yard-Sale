c89924183db1766cf667e071a01e964b154307b5
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2023-07-31 15:54:42 +0100

    Add bundle.js script html

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
-        publicPath: '/',
+        // publicPath: '/',
     },
     mode: 'development',
     resolve: {
