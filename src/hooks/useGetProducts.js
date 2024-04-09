ed3e7043e1d502df21e1147965ec0565d3c3a655
YuneidyC <yuneidycastillo25@gmail.com>
2024-04-09 15:10:27 +0100

    Increase limit of products requested

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "https://api.escuelajs.co/api/v1/products?limit=10&offset=0";
+const API = "https://api.escuelajs.co/api/v1/products?limit=50&offset=0";
 
 const useGetProducts = () => {
     const [products, setProducts] = useState([]);
